770c52597c8e93804ebab7a1e68a181c43913168
Pavel Tysliatski <pavel.tyslyatsky@gmail.com>
2014-08-14 17:43:27 +0300

    make code more pep8, use one type of quotes

--- examples/blog.py
+++ examples/blog.py
@@ -11,13 +11,16 @@ auto = Autodoc(app)
 users = []
 posts = []
 
+
 class User(object):
     def __init__(self, username):
         self.username = username
         users.append(self)
         self.id = users.index(self)
+
     def __repr__(self):
         return dumps(self.__dict__)
+
 
 class Post(object):
     def __init__(self, title, content, author):
@@ -25,60 +28,70 @@ class Post(object):
         self.content = content
         posts.append(self)
         self.id = posts.index(self)
+
     def __repr__(self):
         return dumps(self.__dict__)
 
-u = User("acoomans")
-Post("First post", "This is the first awesome post", u)
-Post("Second post", "This is another even more awesome post", u)
 
-@app.route("/")
-@app.route("/posts")
-@auto.doc(groups=["posts", 'public', "private"])
+u = User('acoomans')
+Post('First post', 'This is the first awesome post', u)
+Post('Second post', 'This is another even more awesome post', u)
+
+
+@app.route('/')
+@app.route('/posts')
+@auto.doc(groups=['posts', 'public', 'private'])
 def get_posts():
     """Returns all posts."""
-    return "%s" % posts
+    return '%s' % posts
+
 
 @app.route('/post/<int:id>')
-@auto.doc(groups=["posts", 'public', "private"])
+@auto.doc(groups=['posts', 'public', 'private'])
 def get_post(id):
     """This returns a post with an id."""
-    return "%s" % posts[id]
+    return '%s' % posts[id]
+
 
 @app.route('/users')
-@auto.doc(groups=["users", 'public', "private"])
+@auto.doc(groups=['users', 'public', 'private'])
 def get_users():
     """This returns all users."""
-    return "%s" % users
+    return '%s' % users
+
 
 @app.route('/user/<int:id>')
-@auto.doc(groups=["users", 'public', "private"])
+@auto.doc(groups=['users', 'public', 'private'])
 def get_user(id):
     """This returns a user with a given id."""
-    return "%s" % users[id]
+    return '%s' % users[id]
 
 
-@app.route('/users', methods=["POST"])
-@auto.doc(groups=["users", 'private'])
+@app.route('/users', methods=['POST'])
+@auto.doc(groups=['users', 'private'])
 def post_user(id):
     """This creates a new user."""
-    User("frank")
-    redirect("/users")
+    User('frank')
+    redirect('/users')
 
-@app.route('/admin', methods=["GET"])
+
+@app.route('/admin', methods=['GET'])
 @auto.doc(groups=['private'])
 def admin():
     """Admin interface."""
-    return "Admin interface"
+    return 'Admin interface'
+
 
 @app.route('/doc')
 @app.route('/doc/public')
 def public_doc():
-    return auto.html(groups=["public"], title="Blog Documentation")
+    return auto.html(groups=['public'], title='Blog Documentation')
+
 
 @app.route('/doc/private')
 def private_doc():
-    return auto.html(groups=["private"], title="Private Documentation")
+    return auto.html(groups=['private'], title='Private Documentation')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run()
--- examples/factory/blog/doc.py
+++ examples/factory/blog/doc.py
@@ -5,11 +5,13 @@ from flask.ext.autodoc import Autodoc
 doc = Blueprint('doc', __name__, url_prefix='/doc')
 auto = Autodoc()
 
+
 @doc.route('/')
 @doc.route('/public')
 def public_doc():
-    return auto.html(groups=["public"], title="Blog Documentation")
+    return auto.html(groups=['public'], title='Blog Documentation')
+
 
 @doc.route('/private')
 def private_doc():
-    return auto.html(groups=["private"], title="Private Documentation")
+    return auto.html(groups=['private'], title='Private Documentation')
