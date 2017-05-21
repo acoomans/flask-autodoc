ea74801222231145421187a005756db752e9a2f8
Jack Grahl <jack.grahl@gmail.com>
2017-05-21 08:51:50 +0000

    Add a test for HTML retrieval.

---
+++ tests/test_flask_get.py
@@ -0,0 +1,26 @@
+import unittest
+
+from flask import Flask
+from flask.ext.autodoc import Autodoc
+
+
+class TestAutodocWithFlask(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.autodoc = Autodoc(self.app)
+
+        @self.app.route('/')
+        @self.autodoc.doc()
+        def index():
+            """Returns a hello world message"""
+            return 'Hello World!'
+
+        self.client = self.app.test_client()
+
+    def test_html(self):
+        @self.app.route('/docs')
+        def html_docs():
+            return self.autodoc.html()
+
+        response = self.client.get('/docs')
+        self.assertEqual(response.status_code, 200)
