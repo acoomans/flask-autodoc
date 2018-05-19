2102a955dd5bdceca00a087e1613e2974b54cf8e
Jack Grahl <jack.grahl@gmail.com>
2018-05-19 20:37:33 +0000

    Make this the first release candidate.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name='Flask-Selfdoc',
-    version='1.0',
+    version='1.0rc1',
     url='http://github.com/jwg4/flask-selfdoc',
     license='MIT',
     author='Arnaud Coomans',
