842441bebc328329caef0a7e7aae6d8594318097
Jack Grahl <jack.grahl@gmail.com>
2018-05-19 22:15:45 +0000

    Add a test that these calls dont fail.

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -10,3 +10,16 @@ class TestErrorHandling(unittest.TestCase):
         autodoc = Autodoc()
         with app.app_context():
             self.assertRaises(RuntimeError, lambda: autodoc.html())
+
+    def test_app_initialized_by_ctor(self):
+        app = Flask(__name__)
+        autodoc = Autodoc(app)
+        with app.app_context():
+            autodoc.html()
+
+    def test_app_initialized_by_init_app(self):
+        app = Flask(__name__)
+        autodoc = Autodoc()
+        autodoc.init_app(app)
+        with app.app_context():
+            autodoc.html()
