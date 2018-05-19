f7c452b5c6648fa4717efc2cb5b2e5b2d4c9509c
Jack Grahl <jack.grahl@gmail.com>
2018-05-19 20:08:49 +0000

    Bump version to 1.0.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name='Flask-Selfdoc',
-    version='1.0a2',
+    version='1.0',
     url='http://github.com/jwg4/flask-selfdoc',
     license='MIT',
     author='Arnaud Coomans',
