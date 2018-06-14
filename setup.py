eadd027076b5b51ed10aaadea2d1b51311503940
jwg4 <jwg4@users.noreply.github.com>
2018-06-14 21:39:52 +0100

    Release 1.1 (#43)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name='Flask-Selfdoc',
-    version='1.0.2',
+    version='1.1.0',
     url='http://github.com/jwg4/flask-selfdoc',
     license='MIT',
     author='Arnaud Coomans',
