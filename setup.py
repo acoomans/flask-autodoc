567df2cd2ef904654d2744d5ac223dbf1dd531a5
Jack Grahl <jack.grahl@gmail.com>
2019-07-15 21:08:57 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name='Flask-Selfdoc',
-    version='1.2.0',
+    version='1.2.1',
     url='http://github.com/jwg4/flask-selfdoc',
     license='MIT',
     author='Arnaud Coomans',
