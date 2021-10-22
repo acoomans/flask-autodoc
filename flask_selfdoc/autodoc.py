5269c87ea7b475998a77f030576028a9ce823feb
Jack Grahl <jack.grahl@gmail.com>
2021-10-22 23:20:38 +0100

    Use a list not a set of args.

--- flask_selfdoc/autodoc.py
+++ flask_selfdoc/autodoc.py
@@ -142,7 +142,7 @@ class Autodoc(object):
                 continue
 
             func = current_app.view_functions[rule.endpoint]
-            arguments = rule.arguments if rule.arguments else ['None']
+            arguments = list(rule.arguments) if rule.arguments else ['None']
             func_groups = self.func_groups[func]
             func_props = self.func_props[func] if func in self.func_props \
                 else {}
