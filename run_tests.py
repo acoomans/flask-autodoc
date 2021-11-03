0c2eadd6f8dd26d9ab1f673136816212d0715fc3
Jack Grahl <jack.grahl@gmail.com>
2021-11-03 00:46:24 +0000

    Check if already uploaded.

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,11 @@
 import doctest
+import logging
 import os
 import subprocess
+
+import requests
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def test():
@@ -11,3 +16,23 @@ def test():
 
 def run_doctest():
     doctest.testfile("README.md")
+
+
+def get_version():
+    version = subprocess.check_output(
+        ['poetry', 'version', '-s']
+    )
+    version = version.decode('utf-8')
+    version = version.strip()
+    logging.debug("Detected version is %s" % (version, ))
+    return version
+
+
+def check_pypi():
+    version = get_version()
+    url = "https://pypi.org/project/flask-selfdoc/%s/" % (version, )
+    r = requests.get(url)
+    if r.status_code == 200:
+        exit(-1)
+    else:
+        exit(0)
